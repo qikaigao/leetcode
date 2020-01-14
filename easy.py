1fc5f083f81624f3c0f0029a50cfc91547e9d8f4
qikaigao <qikaigao2017@gmail.com>
2020-01-13 20:28:49 -0800

    28. Implement strStr()

--- easy.py
+++ easy.py
@@ -4,8 +4,20 @@
 
 
 
-
-
+# 28. Implement strStr()
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "" :
+            return 0
+        if len(needle) > len(haystack):
+            return -1
+        
+        ln = len(needle)
+        lh = len(haystack)
+        for i in range(lh - ln + 1):
+            if haystack[i:i+ln] == needle:
+                return i
+        return -1
 
 
 # 27. Remove Element
