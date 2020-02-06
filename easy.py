ceb47a6e853d7032336a26d6e865415a36fa6872
qikaigao <qikaigao2017@gmail.com>
2020-02-05 23:32:24 -0800

    190. Reverse Bits

--- easy.py
+++ easy.py
@@ -4,6 +4,18 @@
 
 
 
+
+
+# 190. Reverse Bits
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ans = 0
+        for i in range(32):
+            if n&(1<<i):
+                ans += 1<< 31-i
+        return ans
+
+
 # 342. Power of Four
 class Solution:
     def isPowerOfFour(self, num: int) -> bool:
