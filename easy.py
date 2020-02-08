99e80b33e76dda8d2207ddfba80ae356c74010eb
qikaigao <qikaigao2017@gmail.com>
2020-02-08 15:08:36 -0800

    88. Merge Sorted Array

--- easy.py
+++ easy.py
@@ -1,6 +1,28 @@
 
 
 
+
+
+
+
+
+# 88. Merge Sorted Array
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        while n >= 1:
+            i = n+m-1
+            if m <= 0 or nums2[n-1] >= nums1[m-1]:
+                nums1[i] = nums2[n-1]
+                n -= 1
+            else:
+                nums1[i] = nums1[m-1]
+                m -= 1
+
+
+
 # 66. Plus One
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
