cd79f808f880951e7e10985e6cca11c95ccd8735
qikaigao <qikaigao2017@gmail.com>
2020-02-15 01:37:34 -0800

    # 559. Maximum Depth of N-ary Tree BFS

--- easy.py
+++ easy.py
@@ -9,6 +9,31 @@
 
 
 
+
+
+
+
+# 559. Maximum Depth of N-ary Tree    BFS
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        ans = 0
+        q = [root]
+        while q:
+            ans+=1
+            for i in range(len(q)):
+                q+=q.pop(0).children
+        return ans
+
+
 # 559. Maximum Depth of N-ary Tree
 """
 # Definition for a Node.
